4a3bb853fa7bb5ae588dcac137ba89d6af5ff365
Andres Pinzon <ampinzonv@unal.edu.co>
2021-07-26 19:32:38 +0000

    First commit

--- run_offline_nextclade.sh
+++ run_offline_nextclade.sh
@@ -15,18 +15,21 @@
 #
 
 
+resultsDir=${1}
+runId=${2}
+
 nextCladeBin=$(echo "/home/apinzon/mis_datos/Analysis/20210721-vigilancia/tools/soft/nextclade/nextclade-Linux-x86_64")
 nextCladeFiles=$(echo "/home/apinzon/mis_datos/Analysis/20210721-vigilancia/data/other_data/nextclade/")
-consFileName=$(echo ${2}".consensus.fasta")
-
-
+consFileName=$(echo ${runId}".consensus.fasta")
+outputDir=$(echo ${runId}"_nextclade_output")
 
 echo "Gathering consensus files:"
-cat ${1}/medaka/*consensus*  > ${consFileName}
+cat ${resultsDir}/medaka/*consensus*  > ${consFileName}
 
 #Run nextclade
 
-${nextCladeBin} --input-fasta=${consFileName} --input-root-seq=${nextCladeFiles}reference.fasta --genes=E,M,N,ORF1a,ORF1b,ORF3a,ORF6,ORF7a,ORF7b,ORF8,ORF9b,S --input-gene-map=${nextCladeFiles}genemap.gff --input-tree=${nextCladeFiles}tree.json --input-qc-config=${nextCladeFiles}qc.json --input-pcr-primers=${nextCladeFiles}primers.csv --output-json=output/nextclade.json --output-csv=output/nextclade.csv --output-tsv=output/nextclade.tsv --output-tree=output/nextclade.auspice.json --output-dir=output/ --output-basename=${2} 
+${nextCladeBin} --input-fasta=${consFileName} --input-root-seq=${nextCladeFiles}reference.fasta --genes=E,M,N,ORF1a,ORF1b,ORF3a,ORF6,ORF7a,ORF7b,ORF8,ORF9b,S --input-gene-map=${nextCladeFiles}genemap.gff --input-tree=${nextCladeFiles}tree.json --input-qc-config=${nextCladeFiles}qc.json --input-pcr-primers=${nextCladeFiles}primers.csv --output-json=${outputDir}/${runId}_nextclade.json --output-csv=${outputDir}/${runId}_nextclade.csv --output-tsv=${outputDir}/${runId}_nextclade.tsv --output-tree=${outputDir}/${runId}_nextclade.auspice.json --output-dir=${outputDir}/ --output-basename=${2} 
 
-
+#Clean up!
+mv ${consFileName} ${outputDir}
 
